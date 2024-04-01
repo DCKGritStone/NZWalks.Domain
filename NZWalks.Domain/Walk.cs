8f96730e83caa0fddd6847aac04fb3ee34007a82
Planckepoch <Planckepoch@GRIT-CLT-LT-066>
2024-04-01 10:27:33 +0530

    Add project files.

---
+++ NZWalks.Domain/Walk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZWalks.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }= null;
+        public string? Description { get; set; }=null;
+
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; } 
+
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        public Difficulty? Difficulty { get; set; }
+        public Region? Region { get; set; }
+
+    }
+}
